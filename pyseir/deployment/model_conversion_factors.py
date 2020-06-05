017216d6e61a80e72b7328577ed0949865d5a5a6
Brett <brettboval@gmail.com>
2020-06-05 11:58:02 -0700

    Add Test in model_conversion_factors.py

--- pyseir/deployment/model_conversion_factors.py
+++ pyseir/deployment/model_conversion_factors.py
@@ -1,15 +1,16 @@
 import us
 import structlog
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from pyseir import load_data
+from typing import Tuple
 
 log = structlog.get_logger()
 
 UNITY_SCALING_FACTOR = 1
 
 
-def _get_model_to_dataset_conversion_factors_for_state(state, t0_simulation, fips):
+def _get_model_to_dataset_conversion_factors_for_state(state, t0_simulation):
     """
     Return scaling factors to convert model hospitalization and model icu numbers to match
     the most current values provided in combined_datasets.
@@ -18,20 +19,19 @@ def _get_model_to_dataset_conversion_factors_for_state(state, t0_simulation, fip
     ----------
     state
     t0_simulation
-    fips
-
     Returns
     -------
     convert_model_to_observed_hospitalized
     convert_model_to_observed_icu
     """
+    state_obj = us.states.lookup(state)
 
     # Get "Ground Truth" from outside datasets
     # NB: If only cumulatives are provided, we estimate current load. So this isn't strictly
     # actuals from covid-tracking.
-    state_abbreviation = us.states.lookup(state).abbr
+
     days_since_start, observed_latest_hospitalized = load_data.get_current_hospitalized_for_state(
-        state=state_abbreviation,
+        state=state_obj.abbr,
         t0=t0_simulation,
         category=load_data.HospitalizationCategory.HOSPITALIZED,
     )
@@ -54,10 +54,10 @@ def _get_model_to_dataset_conversion_factors_for_state(state, t0_simulation, fip
 
         # Get Compartment Values for a Given Time
         model_state_hosp_gen = load_data.get_compartment_value_on_date(
-            fips=fips, compartment="HGen", date=t_latest_hosp_data_date
+            fips=state_obj.fips, compartment="HGen", date=t_latest_hosp_data_date
         )
         model_state_hosp_icu = load_data.get_compartment_value_on_date(
-            fips=fips, compartment="HICU", date=t_latest_hosp_data_date
+            fips=state_obj.fips, compartment="HICU", date=t_latest_hosp_data_date
         )
 
         # In the model, general hospital and icu hospital are disjoint states. We have to add them
@@ -68,9 +68,7 @@ def _get_model_to_dataset_conversion_factors_for_state(state, t0_simulation, fip
 
         # Now let's look at ICU observed data
         _, observed_latest_icu = load_data.get_current_hospitalized_for_state(
-            state=state_abbreviation,
-            t0=t0_simulation,
-            category=load_data.HospitalizationCategory.ICU,
+            state=state_obj.abbr, t0=t0_simulation, category=load_data.HospitalizationCategory.ICU,
         )
         if observed_latest_icu is None:
             # We have observed hospitalizations, but not observed icu
@@ -92,11 +90,176 @@ def _get_model_to_dataset_conversion_factors_for_state(state, t0_simulation, fip
             return model_to_observed_hospitalized_ratio, model_to_observed_icu_ratio
 
 
-def _get_model_to_dataset_conversion_factors_for_county(state, t0_simulation, fips):
-    """"""
-    return NotImplementedError
+# def _get_model_to_dataset_conversion_factors_for_county(state, t0_simulation, fips, pyseir_outputs):
+#     """
+#     Return scaling factors to convert model hospitalization and model icu numbers to match
+#     the most current values provided in combined_datasets.
+#
+#     Parameters
+#     ----------
+#     t0_simulation
+#     fips
+#     pyseir_outputs
+#
+#     Returns
+#     -------
+#     hosp_rescaling_factor
+#     icu_rescaling_factor
+#     """
+#     # Check if we have county observed data
+#     observed_county_hosp_latest, observed_county_icu_latest = _get_county_hospitalization(fips, t0_simulation)
+#     log.info(
+#         "Actual county hospitalizations",
+#         fips=fips,
+#         hospitalized=observed_county_hosp_latest,
+#         icu=observed_county_icu_latest,
+#     )
+#     if observed_county_hosp_latest is None:
+#         # What do we do if we have no county data? Fall back to the scales from the state?
+#     elif observed_county_hosp_latest == 0:
+#         # Can't scale. Do something else
+#     else:
+#         # If we have county hospitalization data, then we scale the county specific model's
+#         # data to match
+#         model_county_hosp_gen = load_data.get_compartment_value_on_date(
+#             fips=fips,
+#             compartment="HGen",
+#             date=t_latest_hosp_data_date, #TODO: GET THIS
+#             ensemble_results=pyseir_outputs,
+#         )
+#         model_county_icu = load_data.get_compartment_value_on_date(
+#             fips=fips,
+#             compartment="HICU",
+#             date=t_latest_hosp_data_date,
+#             ensemble_results=pyseir_outputs,
+#         )
+#         model_county_heads_in_beds = model_county_hosp_gen + model_county_icu
+#
+#         if observed_county_icu_latest is None:
+
+
+#
+#
+#
+#     state_abbreviation = us.states.lookup(state).abbr
+#     t_latest_hosp_data, current_hosp_count = load_data.get_current_hospitalized_for_state(
+#         state=state_abbreviation,
+#         t0=t0_simulation,
+#         category=load_data.HospitalizationCategory.HOSPITALIZED,
+#     )
+#
+#     _, current_state_icu = load_data.get_current_hospitalized_for_state(
+#         state=state_abbreviation,
+#         t0=t0_simulation,
+#         category=load_data.HospitalizationCategory.ICU,
+#     )
+#
+#     if current_hosp_count is not None:
+#         state_fips = fips[:2]
+#         t_latest_hosp_data_date = t0_simulation + timedelta(days=int(t_latest_hosp_data))
+#
+#         state_hosp_gen = load_data.get_compartment_value_on_date(
+#             fips=state_fips, compartment="HGen", date=t_latest_hosp_data_date
+#         )
+#         state_hosp_icu = load_data.get_compartment_value_on_date(
+#             fips=state_fips, compartment="HICU", date=t_latest_hosp_data_date
+#         )
+#
+#         if len(fips) == 5:
+#             (observed_county_hosp_latest, observed_county_icu_latest,) = self._get_county_hospitalization(
+#                 fips, t0_simulation
+#             )
+#             log.info(
+#                 "Actual county hospitalizations",
+#                 fips=fips,
+#                 hospitalized=observed_county_hosp_latest,
+#                 icu=observed_county_icu_latest,
+#             )
+#             inferred_county_hosp = load_data.get_compartment_value_on_date(
+#                 fips=fips,
+#                 compartment="HGen",
+#                 date=t_latest_hosp_data_date,
+#                 ensemble_results=pyseir_outputs,
+#             )
+#
+#             county_hosp = inferred_county_hosp
+#
+#             inferred_county_icu = load_data.get_compartment_value_on_date(
+#                 fips=fips,
+#                 compartment="HICU",
+#                 date=t_latest_hosp_data_date,
+#                 ensemble_results=pyseir_outputs,
+#             )
+#             log.info(
+#                 "Inferred county hospitalized for fips.",
+#                 fips=fips,
+#                 hospitalized=inferred_county_hosp,
+#                 icu=inferred_county_icu,
+#             )
+#             county_icu = inferred_county_icu
+#             if self._is_valid_count_metric(observed_county_hosp_latest):
+#                 # use actual instead of adjusted
+#                 county_hosp = observed_county_hosp_latest
+#
+#             if self._is_valid_count_metric(observed_county_icu_latest):
+#                 county_icu = observed_county_icu_latest
+#
+#             # Rescale the county level hospitalizations by the expected
+#             # ratio of county / state hospitalizations from simulations.
+#             # We use ICU data if available too.
+#             current_hosp_count *= (county_hosp + county_icu) / (state_hosp_gen + state_hosp_icu)
+#
+#         hosp_rescaling_factor = current_hosp_count / (state_hosp_gen + state_hosp_icu)
+#
+#         # Some states have covidtracking issues. We shouldn't ground ICU cases
+#         # to zero since so far these have all been bad reporting.
+#         if len(fips) == 5 and self._is_valid_count_metric(observed_county_icu_latest):
+#             icu_rescaling_factor = observed_county_icu_latest / inferred_county_icu
+#         elif self._is_valid_count_metric(current_state_icu):
+#             icu_rescaling_factor = current_state_icu / state_hosp_icu
+#         else:
+#             icu_rescaling_factor = current_hosp_count / (state_hosp_gen + state_hosp_icu)
+#     else:
+#         hosp_rescaling_factor = 1.0
+#         icu_rescaling_factor = 1.0
+#     return hosp_rescaling_factor, icu_rescaling_factor
+#
+#     return NotImplementedError
+#
+#
+# def _get_county_hospitalization(fips: str, t0_simulation: datetime) -> Tuple[float, float]:
+#     """
+#     Fetches the latest county hospitalization and icu utilization.
+#
+#     If current data is available, we return that.
+#     If not, current values are estimated from cumulative.
+#     """
+#     county_hosp = load_data.get_current_hospitalized_for_county(
+#         fips, t0_simulation, category=load_data.HospitalizationCategory.HOSPITALIZED,
+#     )[1]
+#     county_icu = load_data.get_current_hospitalized_for_county(
+#         fips, t0_simulation, category=load_data.HospitalizationCategory.ICU
+#     )[1]
+#
+#     return county_hosp, county_icu
 
 
 if __name__ == "__main__":
+    from pyseir.inference.fit_results import load_inference_result
+
+    # Run for California
+    s = "California"
+    fit_results = load_inference_result(us.states.lookup(s).fips)
+    t0_simulation = datetime.fromisoformat(fit_results["t0_date"])
+
+    a, b = _get_model_to_dataset_conversion_factors_for_state("California", t0_simulation)
+    log.info(
+        "Results for model to observed conversion:",
+        state=s,
+        hosp_rescaling_factor=a,
+        icu_rescaling_factor=b,
+    )
+
+    # pyseir_outputs = load_data.load_ensemble_results()
     # Run those unittests
-    pass
+    # pass
